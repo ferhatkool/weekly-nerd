64ea7a31d1c21208e4b405f93a93780130226575
ferhatkool <ferhat.kool@hva.nl>
2024-06-14 11:52:39 +0200

    added the new back-end for the website

---
+++ scripts/data.js
@@ -0,0 +1,122 @@
+export const subjects = {
+    "web-app-from-scratch":
+    {
+        name: "Web App From Scratch",
+        image: "",
+        description: "",
+    },
+    "browser-technologies":
+    {
+        name: "Browser Technologies",
+        image: "",
+        description: "",
+    },
+    "css-to-the-rescue":
+    {
+        name: "CSS-To-The-Rescue",
+        image: "",
+        description: "",
+    },
+    "human-centered-design":
+    {
+        name: "Human Centered Design",
+        image: "",
+        description: "",
+    },
+    "api":
+    {
+        name: "API",
+        image: "",
+        description: "",
+    },
+    "hackathon":
+    {
+        name: "Hackathon",
+        image: "",
+    },
+    "meesterproef":
+    {
+        name: "Meesterproef",
+        image: "",
+    },
+}
+
+export const speakers = {
+    "kilian-valkhof":
+    {
+        name: "Kilian Valkhof",
+        id: "kilian-valkhof",
+        image: "./pictures/kilian_original.jpg",
+        date: "07-02-2024",
+        description: "The rule of least power, move features from JS to HTML and CSS",
+    },
+    "fenna-de-wilde":
+    {
+        name: "Fenna de Wilde",
+        id: "fenna-de-wilde",
+        image: "./pictures/fenna_original.jpg",
+        date: "14-02-2024",
+        description: "Cool & toegankelijk, principes in een 'hostile omgeving'",
+    },
+    "jeffrey-arts":
+    {
+        name: "Jeffrey Arts",
+        id: "jeffrey-arts",
+        image: "./pictures/jeffrey_original.png",
+        date: "28-02-2024",
+        description: "Frontend en kunst, creeër een persoonlijke boiler plate/setup",
+    },
+    "nils-binder":
+    {
+        name: "Nils Binder",
+        id: "nils-binder",
+        image: "./pictures/nils_original.jpg",
+        date: "06-03-2024",
+        description: "Design for Flexibility",
+    },
+    "jeremy-keith":
+    {
+        name: "Jeremy Keith",
+        id: "jeremy-keith",
+        image: "./pictures/jeremy_original.png",
+        date: "14-03-2024",
+        description: "Declarative of imperative code/mindset/culture",
+    },
+    "marieke-en-pim":
+    {
+        name: "Marieke de Hoop & Pim Teeuwisse",
+        id: "marieke-en-pim",
+        image: "./pictures/marieke_original.jpg",
+        date: "03-04-2024",
+        description: "Toegankelijkheid",
+    },
+    "rosa-schuurman":
+    {
+        name: "Rosa Schuurman",
+        id: "rosa-schuurman",
+        image: "./pictures/rosa_original.png",
+        date: "10-04-2024",
+        description: "Creative coding & Circuit bending",
+    },
+}
+
+export const weeklyNerds = {
+    "one":
+    {
+        name: "Weekly Nerd 1",
+        image: "",
+        content: "",
+    },
+    "two":
+    {
+        name: "Weekly Nerd 2",
+        image: "",
+        content: "",
+    },
+    "three":
+    {
+        name: "Weekly Nerd 3",
+        image: "",
+        content: "",
+    }
+}
--- server.js
+++ server.js
@@ -1,12 +1,17 @@
+import * as dotenv from '@tinyhttp/dotenv'
 import { App } from '@tinyhttp/app';
 import { logger } from '@tinyhttp/logger';
+import { Liquid } from 'liquidjs';
 import serveStatic from 'serve-static';
 import path from 'path';
-import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
-import { Liquid } from 'liquidjs';
-import sirv from 'sirv';
+// back-end scripts
+import { subjects, speakers, weeklyNerds } from './scripts/data.js'
+
+const envFile = dotenv.config({ path: 'port.env'})
+var port = process.env.PORT
 
 const app = new App();
 var engine = new Liquid()
@@ -19,21 +24,58 @@ app.set('view engine', 'liquid')
 const __filename = fileURLToPath(import.meta.url); // Convert the URL of the current module to a file path
 const __dirname = dirname(__filename); // Get the directory name of the current module
 
-
-app
-    .use(logger())
-    //.use(serveStatic(path.join(__dirname, 'src')))
-    .use(serveStatic(path.join(__dirname, 'src')))
-    .use('/', sirv('dist/assets'))
-    .listen(7000);
-  
-
-app.get('/', async (req, res) => {
-    
-
-    return res.send(renderTemplate('views/index.liquid', {title: 'Blog'}));
-});
-
 const renderTemplate = (template, data) => {
     return engine.renderFileSync(template, data)
 }
+
+app
+    .use(logger())
+    .use(serveStatic(path.join(__dirname, 'src')))
+    .engine('liquid', engine)
+    .set('views', './views')
+    .set('view engine', 'liquid')
+    .listen(port, () => console.log('Server running on http://localhost:' + port));
+  
+
+app.get('/', async (req, res) => {
+    return res.send(renderTemplate('views/index.liquid', {title: 'Blog'}));
+});
+
+app.get('/subjects/:name', async (req, res) => {
+    const subjectName = req.params.name;
+    const subject = subjects[subjectName];
+    if (subject) {
+        return res.send(renderTemplate('views/subject.liquid', {
+            title: subject.name,
+            subject
+        }));
+    } else {
+        res.status(404).send('Subject not found');
+    }
+});
+
+app.get('/speakers/:name', async (req, res) => {
+    const speakerName = req.params.name;
+    const speaker = speakers[speakerName];
+    if (speaker) {
+        return res.send(renderTemplate('views/speaker.liquid', {
+            title: speaker.name,
+            speaker
+        }));
+    } else {
+        res.status(404).send('Speaker not found');
+    }
+});
+
+app.get('/weekly-nerd/:name', async (req, res) => {
+    const number = req.params.name;
+    const weeklyNerd = weeklyNerds[number];
+    if (weeklyNerd) {
+        return res.send(renderTemplate('views/weekly-nerd.liquid', {
+            title: weeklyNerd.name,
+            weeklyNerd 
+        }));
+    } else {
+        res.status(404).send('Weekly Nerd not found');
+    }
+});
